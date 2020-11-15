b4b3f35b210b884183789ec16ea8edb4c666f186
Gareth Tribello <gareth.tribello@gmail.com>
2020-11-15 12:34:49 +0000

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+
+def average_spin( spins ) :
+  # Your code goes here
+  
+  return 
+
+spins1 = [1,-1,1,1,-1,1,1,1,-1,-1]
+print( average_spin( spins1 ) )
+spins2 = [-1,1,1,1,-1,-1,1,1,1,-1]
+print( average_spin( spins2 ) )
